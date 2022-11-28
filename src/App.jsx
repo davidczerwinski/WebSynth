8104017a97d932f0ff258e2c1625701484daa605
JonkHunkle <88416486+JonkHunkle@users.noreply.github.com>
2022-11-27 17:30:26 -0800

    effects seem to be working!

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,107 @@
 import './App.css'
-import { useState } from 'react';
+import {  useEffect, useState } from 'react';
 import Synth from './components/Synth'
-import Header from './components/Header'
+import Master from './components/Master'
+import Effect from './components/Effect'
 import * as Tone from 'tone'
 import { v4 as uuidv4 } from 'uuid';
 import { Grid,Button } from '@mui/material';
 
 function App() {
+  const [gain, setGain] = useState({})
   const [synthBank, setSynthBank]=useState([])
 
   const newSynth =()=>{
     const synthBankCopy= [...synthBank]
     const synth = new Tone.Synth()
     synth.id= uuidv4()
-    synth.volume.value='-60'
     synth.effectsRack=[]
+    synth.power=false
+    synth.volume.value='-60'
+    synth.chain(gain)
     synthBankCopy.push(synth)
     setSynthBank(synthBankCopy)
   }
   const deleteSynth=(e)=>{
-    const synthBankCopy=synthBank
-     synthBank[e.target.id].disconnect()
-     synthBankCopy.splice(e.target.id,1)
-    setSynthBank([...synthBankCopy])
+    const synthBankCopy=[...synthBank];
+    synthBank[e.target.id].dispose()
+    synthBankCopy.splice(e.target.id,1)
+    setSynthBank(synthBankCopy)
   }
 
-
   const addEffect=(e, id)=>{
-    let synthBankCopy=[...synthBank]
-    const {value} = e.target
-    console.log(id)
+    let synthBankCopy=[...synthBank];
+    let foundSynth = synthBankCopy.find(synth=>{
+      return synth.id===id;
+    })
+    const {value} = e.target;
     if(!value){
-      alert('no effect selected')
+      alert('no effect selected');
     }
     if(value==='reverb'){
-      let reverb = new Tone.Reverb()
-      let foundSynth = synthBankCopy.find(synth=>{
-        console.log('synth in synthback: ',synth.id)
-        console.log('id supplied: ',id)
-        return synth.id==id
-      })
-      foundSynth.effectsRack.push(reverb)
-      console.log('found: ',foundSynth)
-      foundSynth.chain(reverb, Tone.Destination)
-      setSynthBank(synthBankCopy)
+      let reverb = new Tone.Reverb();
+      reverb.wet.value=0
+      reverb.id = uuidv4();
+      reverb.synth_id=foundSynth.id;
+      reverb.power = false
+      foundSynth.effectsRack.push(reverb);
+      setSynthBank(synthBankCopy);
     }
+
+    if(value==='distortion'){
+      let distortion = new Tone.Distortion();
+      distortion.wet.value=0
+      distortion.id = uuidv4();
+      distortion.synth_id=foundSynth.id;
+      distortion.power = false
+      foundSynth.effectsRack.push(distortion);
+    }
+    
+    if(value==='freeverb'){
+      let freeverb = new Tone.Freeverb();
+      freeverb.wet.value=0
+      freeverb.id = uuidv4();
+      freeverb.synth_id=foundSynth.id;
+      freeverb.power = false
+      foundSynth.effectsRack.push(freeverb);
+    }
+    if(value==='bitCrusher'){
+      let crusher = new Tone.BitCrusher();
+      crusher.wet.value=0
+      crusher.id = uuidv4();
+      crusher.synth_id=foundSynth.id;
+      crusher.power = false
+      foundSynth.effectsRack.push(crusher);
+    }
+    if(value==='delay'){
+      let delay = new Tone.FeedbackDelay();
+      delay.wet.value=0
+      delay.id = uuidv4();
+      delay.synth_id=foundSynth.id;
+      delay.power = false
+      foundSynth.effectsRack.push(delay);
+    }
+    
+    foundSynth.chain(...foundSynth.effectsRack, gain);
+    setSynthBank(synthBankCopy);
     
   }
 
+  useEffect(()=>{
+    setGain(new Tone.Gain(0).toDestination())
+  }, [])
 
-console.log(synthBank)
+  console.log(synthBank)
   return (
     <Grid container className="App">
         
-      <Header id='header' newSynth={newSynth}/>
+      <Master id='header' gain={gain} setGain={setGain} newSynth={newSynth}/>
         
         {synthBank&&synthBank.length>0?(
         
           <Grid container className='synthBank' gap={2} >
             {synthBank.map((synth,i)=>{
+
               return (
                 <Grid key={`synth_${i}`} container gap={2} item xs={12} className='synthRack'>
                   <Grid container gap={1} direction='column' xs={1} item className='synthBlock' key={synth.id} >
@@ -66,18 +109,22 @@ console.log(synthBank)
                     <select  id='effect_selection' value='' onChange={e=>addEffect(e,synth.id)}>
                       <option value='' disabled hidden>Effects</option>
                       <option value='reverb'>Reverb</option>
+                      <option value='distortion'>Distortion</option>
+                      <option value='freeverb'>Freeverb</option>
+                      <option value='bitCrusher'>BitCrusher</option>
+                      <option value='delay'>Delay</option>
                     </select>
                     <Button variant='contained' color='error' onClick={e=>deleteSynth(e)} id={i} className='btn'>delete</Button>
                   </Grid>
                   <Grid item xs={2} className='visualizer'>
                     <p>placeholder for visualizer</p>
                   </Grid>
-                  <Grid container item xs={8} className='effectsRack'>
+                  <Grid item xs={8} container alignContent='center' className='effectsRack'>
                     {synth.effectsRack.length>0?(
                       synth.effectsRack.map((effect,i)=>{
                       return (
-                        <Grid item>
-                          <p>{effect.name}</p>
+                        <Grid item> 
+                          <Effect id={effect.id} effect={effect} synth={synth} placement={i} key={`effect_${i}`}/>
                         </Grid>
                       )  
                     })
